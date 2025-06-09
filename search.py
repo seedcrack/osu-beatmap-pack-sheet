1b5b0df99ee774998616f165bf0edcf347f78e9e
seedcrack <j5tanabodee@gmail.com>
2025-06-09 18:47:30 +0700

    search beatmaps from ranked dates

---
+++ search.py
@@ -0,0 +1,37 @@
+from ossapi import Ossapi
+api = Ossapi("35923",'AUXQ6p8FknOFr1BqhXyHNbhJacSR9cNOfyKs7ynR')
+
+has_30_days = [4,6,9,11]
+has_31_days = [1,3,5,7,8,10,12]
+result = []
+print('''-1 : all gamemode
+0 : std
+1 : Taiko
+2 : ctb
+3 : Mania''')
+gamemode = int(input('gamemode : '))
+year = input('year to search : ')
+start_month = int(input('starting month (put a number) : '))
+end_month = int(input('ending month (put a number) : '))
+# sry can't do start and end date, too much of a headache
+
+for i in range(start_month,end_month+1):
+    if i in has_30_days:
+        day_in_month = 30
+    if i in has_31_days:
+        day_in_month = 31
+    if i == 2:
+        day_in_month = 28
+    print(day_in_month)
+    # no I will not do the case for leap year, just edit this to 29 if your input is a leap year
+    for j in range(1,day_in_month+1):
+        search_prompt = f'ranked={year}-{i}-{j}'
+        search_result = api.search_beatmapsets(query=search_prompt, mode=gamemode, category='ranked', explicit_content='show', sort="ranked_asc")
+        total_check = search_result.total
+        if total_check > 50:
+            print(f"{search_prompt} will have missing maps {total_check}")
+        for k in search_result.beatmapsets:
+                result.append(k.id)
+
+with open('search_result.txt', 'w') as f:
+    print(result, file=f)
